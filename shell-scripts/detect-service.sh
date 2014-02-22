2f891fde4ddfab0ebeb3147905f61ab8fb4413ce
MRSC <cloud760607@hotmail.com>
2014-02-22 16:55:39 +0000

    bug fixed
    Signed-off-by: MRSC <cloud760607@hotmail.com>

--- shell-scripts/detect-service.sh
+++ shell-scripts/detect-service.sh
@@ -7,20 +7,20 @@ echo "now , the services of your linux system will be detect!"
 echo -e "the www,ftp,ftp,ssh,and mail will be detect! \n"
 
 testing=`netstat -tuln | grep ":80 "`
-if[ "$testing" != ""]; then
-	echo "www is running in your system."
+if [ "$testing" != "" ]; then
+	echo "www(80 port) is running in your system."
 fi
 testing=`netstat -tuln | grep ":22 "`
-if[ "$testing" != ""]; then
-	echo "ssh is running in your system."
+if [ "$testing" != "" ]; then
+	echo "ssh(22 port) is running in your system."
 fi
 testing=`netstat -tuln | grep ":21 "`
-if[ "$testing" != ""]; then
-	echo "ftp is running in your system."
+if [ "$testing" != "" ]; then
+	echo "ftp(21 port) is running in your system."
 fi
 testing=`netstat -tuln | grep ":25 "`
-if[ "$testing" != ""]; then
-	echo "mail is running in your system."
+if [ "$testing" != "" ]; then
+	echo "mail(25 port) is running in your system."
 fi
 
 
